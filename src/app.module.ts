13f5a2105505dfe5d733577250feb2752e4b5a30
= <BuSheeZy@gmail.com>
2022-07-08 10:01:40 -0500

    ADD: validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from 'class-validator';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
