800e4988bf9822a51b958ab916bdc96f255b27dd
= <BuSheeZy@gmail.com>
2022-07-13 11:46:13 -0500

    better dictionary

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { nanoid } from 'nanoid';
+
+import { customAlphabet } from 'nanoid';
+import { nolookalikes } from 'nanoid-dictionary';
+
+const nanoid = customAlphabet(nolookalikes, 6);
 
 @Injectable()
 export class CommonService {
   randomString() {
-    return nanoid(6);
+    return nanoid();
   }
 }
